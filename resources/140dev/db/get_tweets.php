d40616e687949c6a03bc5654e9bc5159992c9e62
NicoH123 <nico.haubner@gmail.com>
2016-02-04 07:20:33 +0100

    Modify keywords for presentation.

--- resources/140dev/db/get_tweets.php
+++ resources/140dev/db/get_tweets.php
@@ -49,7 +49,7 @@ $stream->db_connect();
 // The keywords for tweet collection are entered here as an array
 // More keywords can be added as array elements
 // For example: array('recipe','food','cook','restaurant','great meal')
-$stream->setTrack(array('ISIS'));
+$stream->setTrack(array('ISIS', 'isis', 'refugeecamp', 'refugeeinflux', 'asylum', 'Syria', 'poverty', 'immigrants', 'refugee', 'bordercontrol', 'refugeesnotwelcome', 'war', 'islamistheproblem', 'refugeecrisis', 'refugeeswelcome'));
 
 // Start collecting tweets
 // Automatically call enqueueStatus($status) with each tweet's JSON data
